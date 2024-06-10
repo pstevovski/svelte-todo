e49fafbc3c42bb74f731a75d5074369a33d52922
Petar Stevovski <petar@codeart.mk>
2024-06-10 15:48:28 +0200

    Added general usecase Modal component

---
+++ src/lib/components/Modal.svelte
@@ -0,0 +1,10 @@
+<div>
+  <div
+    class="bg-slate-800 opacity-40 absolute top-0 left-0 w-full h-full"
+  ></div>
+  <div
+    class="bg-white shadow-lg rounded-[8px] p-6 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[450px] w-full"
+  >
+    <slot></slot>
+  </div>
+</div>
