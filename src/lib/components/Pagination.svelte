9f144d7a85012c11b9e340050c18356b7bf5b2c3
Petar Stevovski <petar@codeart.mk>
2024-06-13 14:03:04 +0200

    Dont display the pagination component if no more than 1 page exists

--- src/lib/components/Pagination.svelte
+++ src/lib/components/Pagination.svelte
@@ -24,31 +24,33 @@
   }
 </script>
 
-<div class="flex items-center gap-1 my-4">
-  <button
-    type="button"
-    class="p-2 rounded-[8px] bg-slate-100 text-sm hover:bg-slate-200 hover:shadow-lg duration-100 disabled:bg-slate-300 disabled:cursor-not-allowed disabled:text-gray-50 disabled:hover:shadow-none"
-    disabled={currentPage === 1}
-    on:click={() => dispatch("handlePageChange", { page: currentPage - 1 })}
-    >Prev</button
-  >
-
-  {#each Array.from({ length: totalPages }, (_, index) => index + 1) as page}
+{#if totalPages > 1}
+  <div class="flex items-center gap-1 my-4">
     <button
       type="button"
-      class={cn(
-        "p-2 px-4 rounded-[8px] bg-slate-100 text-sm hover:bg-slate-200 hover:shadow-lg duration-100",
-        currentPage === page ? "bg-blue-500 text-white hover:bg-blue-500" : ""
-      )}
-      on:click={() => dispatch("handlePageChange", { page })}>{page}</button
+      class="p-2 rounded-[8px] bg-slate-100 text-sm hover:bg-slate-200 hover:shadow-lg duration-100 disabled:bg-slate-300 disabled:cursor-not-allowed disabled:text-gray-50 disabled:hover:shadow-none"
+      disabled={currentPage === 1}
+      on:click={() => dispatch("handlePageChange", { page: currentPage - 1 })}
+      >Prev</button
     >
-  {/each}
 
-  <button
-    type="button"
-    class="p-2 rounded-[8px] bg-slate-100 text-sm hover:bg-slate-200 hover:shadow-lg duration-100 disabled:bg-slate-300 disabled:cursor-not-allowed disabled:text-gray-50 disabled:hover:shadow-none"
-    disabled={currentPage === totalPages}
-    on:click={() => dispatch("handlePageChange", { page: currentPage + 1 })}
-    >Next</button
-  >
-</div>
+    {#each Array.from({ length: totalPages }, (_, index) => index + 1) as page}
+      <button
+        type="button"
+        class={cn(
+          "p-2 px-4 rounded-[8px] bg-slate-100 text-sm hover:bg-slate-200 hover:shadow-lg duration-100",
+          currentPage === page ? "bg-blue-500 text-white hover:bg-blue-500" : ""
+        )}
+        on:click={() => dispatch("handlePageChange", { page })}>{page}</button
+      >
+    {/each}
+
+    <button
+      type="button"
+      class="p-2 rounded-[8px] bg-slate-100 text-sm hover:bg-slate-200 hover:shadow-lg duration-100 disabled:bg-slate-300 disabled:cursor-not-allowed disabled:text-gray-50 disabled:hover:shadow-none"
+      disabled={currentPage === totalPages}
+      on:click={() => dispatch("handlePageChange", { page: currentPage + 1 })}
+      >Next</button
+    >
+  </div>
+{/if}
